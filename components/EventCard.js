7864f639e73bdd1730997bb0742073216cbd94f1
Sonia <sonia.pro.neji@gmail.com>
2024-08-04 20:22:58 +0200

    first commit

---
+++ components/EventCard.js
@@ -0,0 +1,11 @@
+import styles from '../styles/EventCard.module.css';
+
+function EventCard() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default EventCard;
---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Header.module.css';
+
+function Header() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Header;
